e41f0bbd7cab046e465759415ec1a1a5f4d11cf6
Ivan Todorov <erex@abv.bg>
2021-09-23 10:21:31 +0300

    new file

---
+++ Python Basics/First steps in coding/Lab/Task08.py
@@ -0,0 +1,6 @@
+count_dogs = int(input())
+count_animals = int(input())
+price_of_food_for_dogs = 2.5
+price_for_food_for_the_other_animals = 4
+needed_money = (count_dogs * price_of_food_for_dogs) + (count_animals * price_for_food_for_the_other_animals)
+print(f"{needed_money} lv")
