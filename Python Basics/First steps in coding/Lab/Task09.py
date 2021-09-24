2681b5f431d1a5f994113a259e79cabc9d353bde
Ivan Todorov <erex@abv.bg>
2021-09-24 19:03:23 +0300

    new file

---
+++ Python Basics/First steps in coding/Lab/Task09.py
@@ -0,0 +1,8 @@
+area = float(input())
+area_price = 7.61
+price = area * area_price
+discount = 0.18
+discount_price = price * discount
+total_price = price - discount_price
+print(f"The final price is: {total_price} lv.")
+print(f"The discount is: {discount_price} lv.")
