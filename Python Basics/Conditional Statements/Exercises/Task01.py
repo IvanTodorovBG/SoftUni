999c41a324a989de09aa350b0c47bf6956f82a67
Ivan Todorov <erex@abv.bg>
2021-10-05 10:22:37 +0300

    new file

---
+++ Python Basics/Conditional Statements/Exercises/Task01.py
@@ -0,0 +1,9 @@
+time_first = int(input())
+time_second = int(input())
+time_third = int(input())
+
+total_time = time_first + time_second + time_third
+minutes = total_time // 60
+seconds = total_time % 60
+
+print(f"{minutes}:{seconds:02d}")
