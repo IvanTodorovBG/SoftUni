45e231d3106b1c2591f5cf0520a21e6b6ac6e8fa
Ivan Todorov <erex@abv.bg>
2021-11-13 11:20:51 +0200

    new task

---
+++ Python Basics/Nested Loop/Lab/Task03.py
@@ -0,0 +1,8 @@
+n = int(input())
+combination = 0
+for x1 in range(0, n + 1):
+    for x2 in range(0, n + 1):
+        for x3 in range(0, n + 1):
+            if n == x1 + x2 + x3:
+                combination += 1
+print(combination)
