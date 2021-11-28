852f71e75e72a5071e218d7d6b9583f7845e1df0
Ivan Todorov <erex@abv.bg>
2021-11-28 17:28:33 +0200

    new task

---
+++ Python Fundamentals/Basic Syntax, Conditional Statements and Loops/Exercise/Task03.py
@@ -0,0 +1,10 @@
+number = int(input())
+
+if number == 88:
+    print(f"Leo finally won the Oscar! Leo is happy")
+elif number == 86:
+    print(f"Not even for Wolf of Wall Street?!")
+elif number != 88 and number != 86 and number < 88:
+    print(f"When will you give Leo an Oscar?")
+elif number > 88:
+    print(f"Leo got one already!")
